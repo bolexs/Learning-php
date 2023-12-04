a4808674c2c030924cca370ac5bcbb419f7b5410
bolexs <boluwatufeadeojo@gmail.com>
2023-12-04 18:25:19 +0100

    get with kvstore api

---
+++ getApiWithPhp.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/');
+$dotenv->load();
+
+$url = 'https://kvstore.p.rapidapi.com/collections';
+
+$collection_name = 'BolexCollection';
+
+$request_url = $url . '/' . $collection_name;
+
+$curl = curl_init($request_url);
+
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, [
+    'X-RapidAPI-Host: kvstore.p.rapidapi.com',
+    'X-RapidAPI-Key:' .$_ENV["RAPIDAPI_KEY"],
+    'Content-Type: application/json'
+]);
